0b1b24b7837a997436cd9c5dfafc775a0c9b4a63
Miroslav Vanis <vanismir@gmail.com>
2015-03-05 10:35:01 +0100

    graphs

--- src/XMLReader.java
+++ src/XMLReader.java
@@ -8,6 +8,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Iterator;
 
 
@@ -82,50 +86,107 @@ public class XMLReader {
 
     }
 
+    private static Calendar setStart(Calendar cal)
+    {
+        cal.set(Calendar.HOUR, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        return cal;
+    }
 
 
-
-    public static void casovaRada(Document document, String fileNameOutput,int actualFileNumber, int numberOfFiles) throws DocumentException {
+    public static void casovaRada(String fileNameInput,String fileNameOutput,int numberOfFiles) throws DocumentException {
         int carsCount=0;
-        int time=0;
+        Document[] documents = new Document[numberOfFiles];
+        double[][] numCars = new double[numberOfFiles][1440];
         try {
-            String FileNameTime = "cas_"+actualFileNumber+".txt";;
-            FileWriter outFileTime = new FileWriter(FileNameTime);
-            PrintWriter outTime = new PrintWriter(outFileTime);
-            FileWriter outFileGNU = new FileWriter("gnuplot_"+actualFileNumber+".txt");
-            PrintWriter outGNU = new PrintWriter(outFileGNU);
-
-            Element root = document.getRootElement();
-
-            // iterate through child elements of root with element name "foo"
-            for ( Iterator i = root.elementIterator( "interval" ); i.hasNext(); ) {
-                outTime.print(time+"    ");
-                Element foo = (Element) i.next();
-                for ( Iterator j = foo.attributeIterator(); j.hasNext(); ) {
-                    Attribute attribute = (Attribute) j.next();
-                    String name = attribute.getName();
-                    if (name.equals("nVehContrib")) {
-                        double pomoc = Double.valueOf(attribute.getValue());
-                        outTime.println(pomoc);
-                        System.out.println(time+"   "+pomoc);
+            for (int h=0;h<numberOfFiles;h++) {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                Calendar now = Calendar.getInstance();
+                now = setStart(now);
+                File f = new File(fileNameInput.replaceFirst("[.][^.]+$", "") + "_" + h + ".xml");
+                documents[h] = parse(f.getName());
+                String FileNameTime = "pocetvozidelpruh_"+h+".txt";
+                FileWriter outFileTime = new FileWriter(FileNameTime);
+                PrintWriter outTime = new PrintWriter(outFileTime);
+                FileWriter outFileGNU = new FileWriter("gnuplot_"+h+".txt");
+                PrintWriter outGNU = new PrintWriter(outFileGNU);
+                Element root = documents[h].getRootElement();
+                int loop = 0;
+                for ( Iterator i = root.elementIterator( "interval" ); i.hasNext(); ) {
+                    outTime.print(sdf.format(now.getTime())+"    ");
+                    Element foo = (Element) i.next();
+                    for ( Iterator j = foo.attributeIterator(); j.hasNext(); ) {
+                        Attribute attribute = (Attribute) j.next();
+                        String name = attribute.getName();
+                        if (name.equals("nVehContrib")) {
+                            double pomoc = Double.valueOf(attribute.getValue());
+                            outTime.println(pomoc);
+                            numCars[h][loop] = pomoc;
+                            loop++;
+                        }
                     }
+                    now.add(Calendar.MINUTE, 1);
                 }
-            time++;
-            }
-            outGNU.println("set terminal pdf");
-            outGNU.println("set output '"+fileNameOutput+"'");
-            outGNU.println("set xrange [0:1440]");
-            outGNU.println("set yrange [0:60]");
+                outGNU.println("set terminal pdf");
+                outGNU.println("set output '"+fileNameOutput.replaceFirst("[.][^.]+$", "") + "_" + h + ".pdf");
+                outGNU.println("set xrange [0:1440]");
+                outGNU.println("set yrange [0:60]");
           /*  outGNU.println("set style data histogram");
             outGNU.println("set style histogram cluster gap 1");
             outGNU.println("set style fill solid border -1");
             outGNU.println("set boxwidth 0.9");
             outGNU.println("set xtic rotate by -45 scale 0");*/
-            outGNU.println("plot '"+FileNameTime+"' using 1:2 with lines");
+                outGNU.println("plot '"+FileNameTime+"' using 1:2 with lines");
+                outGNU.close();
+                outFileTime.close();
+            }
+
+            String FileNameTime = "pocetvozidelpruh.txt";
+            FileWriter outFileTime = new FileWriter(FileNameTime);
+            PrintWriter outFile = new PrintWriter(outFileTime);
+            double[] sum = new double[1440];
+            Calendar now = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+            now = setStart(now);
+            for(int lane=0;lane<numCars[0].length;lane++)
+            {
+
+                outFile.print(sdf.format(now.getTime())+"    ");
+                for (int f=0;f<numberOfFiles;f++)
+                {
+                    sum[lane] = sum[lane]+numCars[f][lane];
+                    outFile.print(numCars[f][lane]+"    ");
+                }
+                outFile.print(sum[lane]+"   ");
+                outFile.println();
+                now.add(Calendar.MINUTE, 1);
+            }
+
+            outFile.close();
+
+            FileWriter outFileGNU = new FileWriter("gnuplot.txt");
+            PrintWriter outGNU = new PrintWriter(outFileGNU);
+
+            outGNU.println("set terminal pdf");
+            outGNU.println("set output '"+fileNameOutput);
+            outGNU.println("set xdata time");
+            outGNU.println("set timefmt '%H:%M'");
+            outGNU.println("set format x '%H:%M'");
+            outGNU.println("set yrange [0:60]");
+            outGNU.print("plot ");
+            for (int c=0;c<=numberOfFiles;c++) {
+                int column = c + 2;
+                int lane = c + 1;
+                if (c!=numberOfFiles)
+                    outGNU.print("'" + FileNameTime + "' using 1:" + column +" with lines title 'pruh"+c+"', ");
+                else
+                    outGNU.println("'" + FileNameTime + "' using 1:" + column +" with lines title 'součet'");
+            }
+      //      outGNU.println("plot '"+FileNameTime+"' using 1:2 with lines, '"+FileNameTime+"' using 1:3 with lines, '"+FileNameTime+"'using 1:4 with lines, '"+FileNameTime+"' using 1:5 with lines");
             outGNU.close();
-            outFileTime.close();
-
-
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -134,17 +195,13 @@ public class XMLReader {
 
     public XMLReader(String fileNameInput, String fileNameOutput) throws DocumentException {
         int numberOfFiles=0;
-        for (int i=0; i<3;i++)
+        for (int i=0; i<4;i++)
         {
             File f = new File(fileNameInput.replaceFirst("[.][^.]+$", "")+"_"+i+".xml");
             if (f.exists())
                 numberOfFiles++;
-                Document document = parse(f.getName());
-                casovaRada(document, fileNameOutput.replaceFirst("[.][^.]+$", "")+"_"+i+".pdf",i,0);
-            
-
         }
-
+        casovaRada(fileNameInput,fileNameOutput,numberOfFiles);
 
 /*           Document document = parse(fileNameInput);
            bar(document, fileNameOutput);*/
